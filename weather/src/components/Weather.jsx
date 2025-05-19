83e7949b1ac2e45f28bbc5e0d89f59738193759a
liesse97 <ishimwelieish@gmail.com>
2025-05-19 17:06:54 +0200

    'start_weather_app'

---
+++ weather/src/components/Weather.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Weather.css'
+import search_icon from '../assets/search.png'
+import clear from '../assets/clear.png'
+import cloud from '../assets/cloud.png'
+import drizzle from '../assets/drizzle.png'
+import humidity from '../assets/humidity.png'
+import rain from '../assets/rain.png'
+import snow from '../assets/snow.png'
+import wind from '../assets/wind.png'
+
+
+
+
+ 
+
+const Weather = () =>{
+    return (
+        <div className="weather">
+        <div className="search-bar">
+            <input type ="text" placeholder='Search' />
+            <img src={search_icon} alt="" />       
+        </div>
+
+            <img src={clear} alt=""  className='weather-icon'/>
+            <p className='temperature'>16°C</p>
+            <p className='location'>London</p>
+            <div className="weather-data">
+                <div className="col">
+                    <div>
+                    <img src={humidity} alt="" />
+                    <p>91%</p>
+                    <span>Humidity</span>
+                </div>
+                </div>
+                <div className="col">
+                    <div>
+                    <img src={wind} alt="" />
+                    <p>3.6 km</p>
+                    <span>Wind speed</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Weather
